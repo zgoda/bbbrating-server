6809f9b2b34ce4bf056724b037bbc31b7fd0165e
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-09-09 23:19:57 +0200

    User item resource

--- src/bbbr/app.py
+++ src/bbbr/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from pony.flask import Pony
 
-from .resource.user import user_collection
+from .resource.user import user_collection, user_item
 
 
 def make_app():
     app = Flask(__name__.split('.')[0])
     Pony(app)
     app.add_url_rule('/users', 'user.collection', view_func=user_collection)
+    app.add_url_rule('/user/<int:user_id>', 'user.item', view_func=user_item)
     return app
--- src/bbbr/resource/user.py
+++ src/bbbr/resource/user.py
@@ -1,4 +1,4 @@
-from flask import Response, jsonify, request
+from flask import Response, jsonify, request, url_for
 from flask.views import MethodView
 
 from ..models import User, db
@@ -8,17 +8,30 @@ from ..schema import UserCreateSchema, user_schema
 class UserCollection(MethodView):
 
     def get(self):
-        return jsonify({'users': user_schema.dump(User.select(), many=True)})
+        users = User.select(lambda u: u.is_active is True)
+        return jsonify({'users': user_schema.dump(users, many=True)})
 
     def post(self):
         sc = UserCreateSchema()
         user_data = sc.load(request.json)
-        User(
+        user = User(
             name=user_data['name'], email=user_data['email'],
             password=User.gen_password(user_data['password']),
         )
         db.flush()
-        return Response(status=201)
+        return Response(
+            status=201, headers={'Location': url_for('user.item', id=user.id)}
+        )
 
 
 user_collection = UserCollection.as_view('user_collection')
+
+
+class UserItem(MethodView):
+
+    def get(self, user_id: int):
+        user = User.get(id=user_id)
+        return jsonify(user_schema.dump(user))
+
+
+user_item = UserItem.as_view('user_item')
