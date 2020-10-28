94d7573344103fe4f9b16c1233eebf05ac0afc1c
Jarek Zgoda <jarek.zgoda@gmail.com>
2020-10-28 20:01:25 +0100

    RW schemas for Beer and Brewery models

--- src/bbbr/schema.py
+++ src/bbbr/schema.py
@@ -12,9 +12,10 @@ class UserSchema(Schema):
     email = fields.Email(required=True)
     name = fields.Str()
     ratings = fields.Nested(
-        'RatingSchema', only=['id', 'beer', 'date', 'overall'], many=True
+        'RatingSchema', only=['id', 'beer', 'date', 'overall'], many=True,
+        dump_only=True,
     )
-    is_active = fields.Bool(data_key='isActive', dump_only=True)
+    is_active = fields.Bool(data_key='isActive')
     reg_date = fields.DateTime(data_key='regDate', dump_only=True)
 
 
@@ -41,8 +42,8 @@ user_login_schema = UserAuthSchema()
 class RatingSchema(Schema):
     id = fields.Integer(dump_only=True)
     date = fields.DateTime(dump_only=True)
-    user = fields.Nested(UserSchema, only=['id', 'name'])
-    beer = fields.Nested('BeerSchema', only=['id', 'name'])
+    user = fields.Nested(UserSchema, only=['id', 'name'], dump_only=True)
+    beer = fields.Nested('BeerSchema', only=['id', 'name'], dump_only=True)
     colour = fields.Integer(required=True)
     colour_text = fields.Str(required=True, data_key='colourText')
     foam = fields.Integer(required=True)
@@ -65,35 +66,32 @@ rating_schema = RatingSchema()
 
 
 class BrewerySchema(Schema):
+    id = fields.Integer(dump_only=True)
     name = fields.Str(required=True)
     town = fields.Str(required=True)
-    beers = fields.Nested('BeerSchema', only=['id', 'name'], many=True)
+    beers = fields.Nested('BeerSchema', only=['id', 'name'], many=True, dump_only=True)
     beers_brewed = fields.Nested(
-        'BeerSchema', only=['id', 'name'], many=True, data_key='beersBrewed'
+        'BeerSchema', only=['id', 'name'], many=True, data_key='beersBrewed',
+        dump_only=True,
     )
     is_active = fields.Boolean(data_key='isActive')
     is_contract = fields.Boolean(data_key='isContract')
-
-    class Meta:
-        additional = ['id']
 
 
 brewery_schema = BrewerySchema()
 
 
 class BeerSchema(Schema):
+    id = fields.Integer(dump_only=True)
     name = fields.Str(required=True)
-    brewery = fields.Nested(BrewerySchema, only=['id', 'name', 'town'])
+    brewery = fields.Nested(BrewerySchema, only=['id', 'name', 'town'], dump_only=True)
     brewed_at = fields.Nested(
-        BrewerySchema, only=['id', 'name', 'town'], data_key='brewedAt'
+        BrewerySchema, only=['id', 'name', 'town'], data_key='brewedAt', dump_only=True
     )
     ratings = fields.Nested(
-        RatingSchema, only=['id', 'date', 'overall', 'user'], many=True
+        RatingSchema, only=['id', 'date', 'overall', 'user'], many=True, dump_only=True
     )
     is_active = fields.Boolean(data_key='isActive')
-
-    class Meta:
-        additional = ['id']
 
 
 beer_schema = BeerSchema()
